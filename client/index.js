0024693267a2e13f0f0c6ce5ab73c11151297170
Montii <llh9@yahoo.com>
2022-04-17 15:25:45 -0400

    new setup

--- client/index.js
+++
@@ -1,7 +0,0 @@
-const sngBtn = document.getElementsByClassName('songBtn');
-
-sngBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    console.log('from js')
-    window.location.href = '/song1.html';
-});
